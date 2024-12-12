cfdf23dcf07797a213db040743d968b106bb9d50
OnlyTravis <TravisTang0626@gmail.com>
2024-12-12 15:00:39 +0800

    did stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -20,6 +25,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ListNumber extends StatelessWidget {
+  final int number;
+
+  const ListNumber({super.key, required this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(number.toString());
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -30,30 +46,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<int> test_list = [1,2,3,3,4,1,41,2];
+
+  String display_text = "aa";
+  bool is_playing = false;
+  AudioPlayer audio_player = AudioPlayer();
+
+  Future<bool> requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      PermissionStatus result = await permission.request();
+      return (result == PermissionStatus.granted);
+    }
+  }
+
+  void _testButton() async {
+    Directory? music_directory;
+    if (await Permission.manageExternalStorage.request() == PermissionStatus.granted) {
+      music_directory = await getExternalStorageDirectory();
+      if (music_directory == null) return;
+      setState(() {
+        display_text = music_directory!.path;
+      });
+    } else {
+      print("Permission Request Failed");
+      setState(() {
+        display_text = "Error";
+      });
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
-    bool is_playing = false;
-    AudioPlayer audio_player = AudioPlayer();
-    audio_player.setSource(AssetSource("./a.m4a"));
 
-  void _testButton() {
-    if (is_playing) {
-      audio_player.pause();
-    } else {
-      audio_player.resume();
-    }
-    is_playing = !is_playing;
-  }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: TextButton(
-          onPressed: _testButton, 
-          child: const Text("Press me"),  
-        )
+      body: Column(
+        children: [...test_list.map((n) => ListTile(
+          title: Center(
+            child: Text(n.toString()),
+          ),
+          tileColor: const Color.fromARGB(255, 222, 255, 253),
+        )), ListTile(
+          title: Center(
+            child: Text(display_text),
+          ),
+          tileColor: const Color.fromARGB(255, 222, 255, 253),
+        )],
+      ),
+      floatingActionButton: TextButton(
+        onPressed: _testButton, 
+        style: TextButton.styleFrom(
+          foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: Colors.lightBlue
+        ),
+        child: Text("Press Me"),
       ),
     );
   }
